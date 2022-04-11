115a61062ca9a21b5e9da18ab3e6dc1c5ef35d77
gautam kumar <gautam01kr11@gmail.com>
2022-04-11 20:03:11 +0530

    gautam updated

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ const express = require('express');
 var bodyParser = require('body-parser');
 
 const route = require('./routes/route.js');
+const mongoose = require('mongoose');
+
+const router = express.Router();
+mongoose.connect('mongodb+srv://gautamkumar1111:Sae755%40gautam@cluster0.v6fgc.mongodb.net/gautam01', { useNewUrlParser: true })
+.then(() => console.log('Connected to MongoDB...'))
+.catch(err => console.error('Could not connect to MongoDB...', err));
+
 
 const app = express();
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,17 +1,15 @@
 const express = require('express');
-const player = require('../playerlist/players');
-
+const userModel = require("../models/userModal")
+const userController = require("../controllers/userController")
 const router = express.Router();
 
-router.post('/test-post1', function (req, res) {
+router.get('/test-me', function (req, res) {
 
     res.send( {  msg: "hi guys"  }   )
 });
 
+router.post('/createUser', userController.createUser);
 
-// to send data in  post request-> prefer sending in BODY -> click body-raw-json
-router.post('/test-post2', player.playersLists);
-
-
+router.get('/getUsersData', userController.getUsersData);
 
 module.exports = router;
